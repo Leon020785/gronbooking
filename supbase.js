ebd014f00c396b1c0814a075b6fe48bf924b4fdf
Leon020785 <leonlorenzen85@gmail.com>
2025-10-16 18:18:24 +0200

    Update Supabase URL and key

--- supbase.js
+++ supbase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'https://xxyucwbjpxfckeeaxfuz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4eXVjd2JqcHhmY2tlZWF4ZnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NDcyOTEsImV4cCI6MjA3NjEyMzI5MX0.EV2tqYtoLOKatUv2hJQa_9ypziafvGiXIabBwi0YEqM'
+const supabaseUrl = 'https://yihspabqegfyzrvxondj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpaHNwYWJxZWdmeXpydnhvbmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MjE5ODksImV4cCI6MjA3NjA5Nzk4OX0.SvhSoaV5J-dcsp4_pmt4LMJSeYyvdJ0XMc88Wo57yQ8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
